4e584fe6ec1ffbe7614e6573581a5c0776b3ce35
Ryan Murphy <ryanm@vogelheating.com>
2024-02-12 14:27:55 -0600

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,31 @@ import './components/styles/vg-utility.css'
 
 function TestApp() {
 
-    function TestFunction(){
+    function TestFunction() {
         console.log('TEST FIRE');
-        window.ipcRenderer.invoke('TEST-CALL').then(resp=>{
+        window.ipcRenderer.invoke('TEST-CALL').then(resp => {
             console.log(resp)
         })
     }
 
     return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'space-around'
-        }}>
-            <button
-                className="create-button flat-action-button"
-                onClick={(eve) => TestFunction()}
-            >CREATE</button>
-            <button
-                className="dl-button flat-action-button"
-                onClick={() => console.log('TEST')}
-                disabled={true}
-            >DOWNLOAD</button>
-        </div>
+        <>
+            <script src='./inject.js'></script>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-around'
+            }}>
+                <button
+                    className="create-button flat-action-button"
+                    onClick={(eve) => TestFunction()}
+                >CREATE</button>
+                <button
+                    className="dl-button flat-action-button"
+                    onClick={() => console.log('TEST')}
+                    disabled={true}
+                >DOWNLOAD</button>
+            </div>
+        </>
     )
 }
 
